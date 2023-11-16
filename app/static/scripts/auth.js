0560c2fe7a50c0c47d19d17210c333500f794f62
deadaf <gahlyanrohit18@gmail.com>
2023-11-16 14:37:02 +0530

    feat: show case details

--- app/static/scripts/auth.js
+++ app/static/scripts/auth.js
@@ -13,7 +13,7 @@ async function handleSignup() {
 		walletAddress = await connectToMetaMask();
 	} else {
 		const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
-		const walletAddress = accounts[0];
+		walletAddress = accounts[0];
 	}
 
 	// Get form data
@@ -40,10 +40,12 @@ async function handleSignup() {
 			// Successfully created user, now make a GET request to retrieve user details
 			await handleLogin();
 		} else {
-			console.error("Error creating user:", response.status, response.statusText);
+			location.reload(true);
+			alert("Already registered with this wallet address, Please Login.");
 		}
 	} catch (error) {
-		console.error("Error during signup:", error);
+		location.reload(true);
+		alert("Already registered with this wallet address, Please Login.");
 	}
 }
 
@@ -52,7 +54,6 @@ async function handleLogin() {
 		await connectToMetaMask();
 	}
 
-	// Make a GET request to /users/{wallet_address}
 	try {
 		const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
 		const walletAddress = accounts[0];
@@ -61,15 +62,88 @@ async function handleLogin() {
 		if (response.ok) {
 			const userData = await response.json();
 
+			document.getElementById("signupForm").style.display = "none";
+			document.getElementById("authBtns").style.display = "none";
 			// Update UI with user details
 			document.getElementById("userData").style.display = "block";
 			document.getElementById("userName").innerText = `Name: ${userData.name}`;
 			document.getElementById("userEmail").innerText = `Email: ${userData.email}`;
 			document.getElementById("userType").innerText = `User Type: ${userData.userType}`;
+
+			// Make a GET request to /users/{wallet_address}/cases
+			const casesResponse = await fetch(`/users/${walletAddress}/cases`);
+			if (casesResponse.ok) {
+				const casesData = await casesResponse.json();
+				displayCases(casesData);
+			} else {
+				console.error("Error retrieving cases:", casesResponse.status, casesResponse.statusText);
+			}
 		} else {
 			console.error("Error retrieving user details:", response.status, response.statusText);
 		}
 	} catch (error) {
 		console.error("Error during login:", error);
 	}
+}
+
+function displayCases(casesData) {
+	// Show the cases table
+	const casesTable = document.getElementById("casesTable");
+	casesTable.style.display = "block";
+
+	// Clear existing table content
+	casesTable.innerHTML = "";
+
+	// Create table headers
+	const tableHeaders = "<tr><th>Case ID</th><th>Title</th><th>Type</th><th>Status</th></tr>";
+	casesTable.innerHTML += tableHeaders;
+
+	// Populate table rows with case data
+	casesData.forEach((caseInfo) => {
+		const row = `<tr onclick="showCaseDetails(${caseInfo})">
+                        <td>${caseInfo.caseId}</td>
+                        <td>${caseInfo.title}</td>
+                        <td>${caseInfo.caseType}</td>
+                        <td>${caseInfo.status}</td>
+                    </tr>`;
+		casesTable.innerHTML += row;
+	});
+
+	// Add event listener to all rows
+	const rows = casesTable.getElementsByTagName("tr");
+	console.log(rows);
+	for (let i = 1; i < rows.length; i++) {
+		rows[i].addEventListener("click", () => {
+			showCaseDetails(casesData[i - 1]);
+		});
+	}
+}
+
+function showCaseDetails(caseInfo) {
+	console.log("Showing case details for case ID:", caseInfo);
+
+	// Create popup element
+	const popup = document.createElement("div");
+	popup.classList.add("popup");
+
+	// Create popup content
+	const content = `
+		<h2>${caseInfo.title}</h2>
+		<p><strong>Case ID:</strong> ${caseInfo.caseId}</p>
+		<p><strong>Type:</strong> ${caseInfo.caseType}</p>
+		<p><strong>Status:</strong> ${caseInfo.status}</p>
+	`;
+
+	// Add content to popup
+	popup.innerHTML = content;
+
+	// Add popup to page
+	document.body.appendChild(popup);
+
+	// Add event listener to close popup when clicked outside
+	popup.addEventListener("click", (event) => {
+		if (event.target === popup) {
+			popup.remove();
+		}
+	});
 }

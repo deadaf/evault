534f1ba49bb284fe76b143a04b50bcedbca09e21
deadaf <gahlyanrohit18@gmail.com>
2023-11-17 00:59:46 +0530

    feat: enable cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,8 +3,21 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from app.routers import users, cases
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 templates = Jinja2Templates(directory="app/templates")
 app.mount("/static", StaticFiles(directory="app/static", html=True), name="static")
 

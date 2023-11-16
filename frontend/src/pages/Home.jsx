175f4bb145342836ef697a6896c21102e200e891
deadaf <gahlyanrohit18@gmail.com>
2023-11-17 04:25:26 +0530

    style: Home Page btns

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,11 +5,15 @@ const Home = () => {
     <>
       <div className="flex flex-col gap-3">
         <Link to="/login">
-          <button className="p-4">Login using MetaMask</button>
+          <button className="rounded-md bg-blue-500 p-4 text-white hover:bg-blue-700 focus:border-blue-300 focus:outline-none focus:ring">
+            Login using MetaMask
+          </button>
         </Link>
 
         <Link to="/signup">
-          <button className="p-4">Sign Up, If you are New!</button>
+          <button className="rounded-md bg-green-500 p-4 text-white hover:bg-green-700 focus:border-green-300 focus:outline-none focus:ring">
+            Sign Up, If you are New!
+          </button>
         </Link>
       </div>
     </>

42a0bdd40e9c89d0ff5bac480c865117f5cbd136
deadaf <gahlyanrohit18@gmail.com>
2023-11-27 23:13:54 +0530

    feat: add pre validator to File model

--- backend/app/models/file.py
+++ backend/app/models/file.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+from datetime import datetime
 
 __all__ = ("File",)
 
@@ -9,4 +10,8 @@ class File(BaseModel):
     caseId: int
     fileAddress: str
     secretKey: str
-    createdAt: str
+    createdAt: str = None
+
+    @validator("created_at", pre=True)
+    def default_created_at(cls, v):
+        return v or datetime.now().isoformat()

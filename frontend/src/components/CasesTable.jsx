3c5e739b537db6b972ef99b8d7a7759851872ad6
deadaf <gahlyanrohit18@gmail.com>
2023-12-01 13:32:45 +0530

    feat: add download encrypted file btn

--- frontend/src/components/CasesTable.jsx
+++ frontend/src/components/CasesTable.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 import { BACKEND_URL } from "../consts";
-
+import BrowseFilesBtn from "./browseFilesBtn";
 
 const CasesTable = ({ user }) => {
   const [cases, setCases] = useState([]);
@@ -19,9 +19,47 @@ const CasesTable = ({ user }) => {
     fetchCases();
   }, [user.walletAddress]);
 
+  const showCaseInfo = (caseInfo) => {
+    // Implement logic to show case info popup
+    console.log("Show Case Info:", caseInfo);
+  };
+
   return (
     <>
-      <div>{cases.length}</div>
+      <div className="overflow-x-auto">
+        <table className="min-w-full table-auto border-collapse">
+          <thead>
+            <tr>
+              <th className="border p-2 text-white">#</th>
+              <th className="border p-2 text-white">Case Title</th>
+              <th className="border p-2 text-white">Show Case Info</th>
+              <th className="border p-2 text-white">Browse Files</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cases.map((caseItem, index) => (
+              <tr key={caseItem.caseId}>
+                <td className="border p-2 text-white">{index + 1}</td>
+                <td className="border p-2 text-white">{caseItem.title}</td>
+                <td className="border p-2 text-white">
+                  <button
+                    onClick={() => showCaseInfo(caseItem)}
+                    className="rounded bg-blue-500 p-2 text-white hover:bg-blue-700"
+                  >
+                    Show Info
+                  </button>
+                </td>
+                <td className="border p-2">
+                  <BrowseFilesBtn
+                    walletAddress={user.walletAddress}
+                    caseId={caseItem.caseId}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
---
+++ frontend/src/components/browseFilesBtn.jsx
@@ -0,0 +1,154 @@
+/* eslint-disable react/prop-types */
+
+import { useState } from "react";
+import { BACKEND_URL } from "../consts";
+
+const BrowseFilesBtn = ({ walletAddress, caseId }) => {
+  const [showPopup, setShowPopup] = useState(false);
+  const [files, setFiles] = useState([]);
+
+  const onClick = async () => {
+    try {
+      const res = await fetch(`${BACKEND_URL}/cases/${caseId}/files`, {
+        headers: {
+          walletAddress: walletAddress,
+        },
+      });
+
+      if (res.ok) {
+        const data = await res.json();
+        setFiles(data);
+        setShowPopup(true);
+      } else {
+        console.error("Error fetching files:", res.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching files:", error);
+    }
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
+
+  const addNewFile = async (event) => {
+    const selectedFile = event.target.files[0];
+
+    if (selectedFile) {
+      try {
+        const formData = new FormData();
+        formData.append("file", selectedFile);
+
+        const res = await fetch(`${BACKEND_URL}/cases/${caseId}/files`, {
+          method: "PUT",
+          headers: {
+            walletAddress: walletAddress,
+          },
+          body: formData,
+        });
+
+        if (res.ok) {
+          // Fetch files again to get the updated list
+          const updatedFilesRes = await fetch(
+            `${BACKEND_URL}/cases/${caseId}/files`,
+            {
+              headers: {
+                walletAddress: walletAddress,
+              },
+            },
+          );
+
+          if (updatedFilesRes.ok) {
+            const updatedFilesData = await updatedFilesRes.json();
+            setFiles(updatedFilesData);
+          }
+        } else {
+          console.error("Error adding new file:", res.statusText);
+        }
+      } catch (error) {
+        console.error("Error adding new file:", error);
+      }
+    }
+  };
+
+  const downloadFile = async (fileId) => {
+    console.log(fileId);
+    try {
+      const fileToDownload = files.find((file) => file.fileId === fileId);
+
+      if (fileToDownload) {
+        const res = await fetch(
+          `${BACKEND_URL}/cases/${caseId}/files/${fileId}`,
+          {
+            headers: {
+              walletAddress: walletAddress,
+            },
+          },
+        );
+        const data = await res.blob();
+
+        const url = window.URL.createObjectURL(new Blob([data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", fileToDownload.name);
+        document.body.appendChild(link);
+        link.click();
+        link.parentNode.removeChild(link);
+      } else {
+        console.error("File not found in the list");
+      }
+    } catch (error) {
+      console.error("Error downloading file:", error);
+    }
+  };
+  return (
+    <div className="relative inline-block">
+      <button
+        onClick={onClick}
+        className="rounded bg-green-500 p-2 text-white hover:bg-green-700"
+      >
+        Browse Files
+      </button>
+
+      {showPopup && (
+        <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform bg-white p-10 shadow-md">
+          <h2 className="mb-4 text-lg font-semibold">
+            Files for Case ID: {caseId}
+          </h2>
+          <ul>
+            {files.map((file, index) => (
+              <li key={index}>
+                {index + 1}. {file.name}{" "}
+                <button
+                  onClick={() => downloadFile(file.fileId)}
+                  className="ml-2 rounded bg-blue-500 p-1 text-xs text-white hover:bg-blue-700 "
+                >
+                  Download
+                </button>
+              </li>
+            ))}
+          </ul>
+          <div className="mt-4 flex justify-between gap-2">
+            <label className="cursor-pointer rounded bg-blue-500 p-2 text-white hover:bg-blue-700">
+              Add New File
+              <input
+                type="file"
+                accept="*" // accepted file types
+                className="hidden"
+                onChange={addNewFile}
+              />
+            </label>
+            <button
+              onClick={closePopup}
+              className="rounded bg-gray-500 p-2 text-white hover:bg-gray-700"
+            >
+              Close Popup
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default BrowseFilesBtn;

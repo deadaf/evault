9f4ec15d4ae524f566c5b9134760bcb9e1c8d327
deadaf <gahlyanrohit18@gmail.com>
2023-12-02 02:59:24 +0530

    feat: create return to Home button.

---
+++ frontend/src/components/goHome.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const GoHome = () => {
+  return (
+    <div className="fixed left-0 top-0 m-4 ml-10 mt-10">
+      <Link to="/">
+        <button className="rounded-md bg-blue-500 p-4 text-white hover:bg-blue-700 focus:border-blue-300 focus:outline-none focus:ring">
+          Return To Home
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default GoHome;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/prop-types */
 import CasesTable from "../components/CasesTable";
 import CreateCaseBtn from "../components/createCaseBtn";
+import GoHome from "../components/goHome";
 
 const Dashboard = ({ user }) => {
   return (
     <div className="container mx-auto mt-8">
+      <GoHome />
       <h2 className="mb-4 text-2xl font-bold text-white">
         Welcome, {user.name} ({user.userType})!
       </h2>

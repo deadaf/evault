086ca40279f3e65c49ab7c06b50f83d9b9dda054
deadaf <gahlyanrohit18@gmail.com>
2023-12-02 03:04:47 +0530

    style: add return symbol to btn

--- frontend/src/components/goHome.jsx
+++ frontend/src/components/goHome.jsx
@@ -5,7 +5,7 @@ const GoHome = () => {
     <div className="fixed left-0 top-0 m-4 ml-10 mt-10">
       <Link to="/">
         <button className="rounded-md bg-blue-500 p-4 text-white hover:bg-blue-700 focus:border-blue-300 focus:outline-none focus:ring">
-          Return To Home
+          ⏎ Return To Home
         </button>
       </Link>
     </div>
